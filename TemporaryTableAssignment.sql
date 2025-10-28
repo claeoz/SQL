2caeff6b98dbd2e33da9edc732df252287d36f1f
Clae M Oczachowski <claemiller@gmail.com>
2025-10-27 21:14:06 -0500

    question 2 is giving me troubles

--- TemporaryTableAssignment.sql
+++ TemporaryTableAssignment.sql
@@ -2,10 +2,35 @@
 Select top 0 *
 Into #Table1
 From Sales.Orders
-Select * From #Table1
+Select * From #Table1;
 
+--
+Select Top 10 * From #Table1
+select Top 1 * from Sales.OrderDetails;
+--
 --2
+Insert Into #Table1
+Select 
+	o.orderid, 
+	o.custid, 
+	o.empid, 
+	o.orderdate, 
+	o.requireddate, 
+	o.shippeddate, 
+	o.shipperid, 
+	o.freight,
+	o.shipname, 
+	o.shipaddress, 
+	o.shipcity, 
+	o.shipregion, 
+	o.shippostalcode, 
+	o.shipcountry
+From Sales.Orders o
+Join Sales.OrderDetails d on o.orderid = d.orderid
+Where d.discount = 0
+Set IDENTITY_INSERT #Table1 OFF;
 
+Select Top 10 * From #Table1
 --3
 
 --4
